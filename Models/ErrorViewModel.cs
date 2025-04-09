38efb80c3e0a84007df2dcceb18ec451f3824b9a
Matthew Barhou <mbarhou@my.centennialcollege.ca>
2025-04-09 12:05:12 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace matt_quiz4_agile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
